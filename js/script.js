6e6dc051b95dccf01940f59a585658fa161160b6
Szymon Leyk <leyas@wp.pl>
2019-12-18 18:09:13 +0100

    rozpoczęcie next na podstawie DOM

--- js/script.js
+++ js/script.js
@@ -1,19 +1,40 @@
 function displayPrev() {
-  console.log("Wywołałem funkcję displayPrev")
-  var slide1 = document.getElementById("slide1");
-  var sadfasdfasdf = document.getElementById("slide2");
 
-  slide1.style.display = "block";
-  sadfasdfasdf.style.display = "none";
+
+
+
+  // console.log("Wywołałem funkcję displayPrev")
+  // var slide1 = document.getElementById("slide1");
+  // var sadfasdfasdf = document.getElementById("slide2");
+  //
+  // slide1.style.display = "block";
+  // sadfasdfasdf.style.display = "none";
 }
 
 function displayNext() {
-  var slide1 = document.getElementById("slide1");
-  var slide2 = document.getElementById("slide2");
+  // który jest obecnym blokiem ?
+  var actualTable = document.getElementsByClassName("visible");
+  var actual = actualTable[0];
 
-  slide1.style.display = "none";
-  slide2.style.display = "block";
-  slide2.style.backgroundColor = "yellow";
+  console.log(actual);
+
+  // na podstawie obecnego oblicz kto jest kolejny // slide1 -> slide2
+  var actualId = actual.id;
+
+  // var next = actual.id;
+
+  console.log(number);
+
+  // ukryj obecny // dodaj klase hidden
+  //
+  // odkryj nastepny // usuń klasę hidden
+
+  // var slide1 = document.getElementById("slide1");
+  // var slide2 = document.getElementById("slide2");
+  //
+  // slide1.style.display = "none";
+  // slide2.style.display = "block";
+  // slide2.style.backgroundColor = "yellow";
 
 
 }
